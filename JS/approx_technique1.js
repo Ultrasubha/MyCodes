9700c3b0b05b7a78f1dbc4b2ed1003f56462a4ca
Subhadeep Mandal <ultrasubha@gmail.com>
2021-03-29 22:07:39 +0530

    Simple approx technique

---
+++ JS/approx_technique1.js
@@ -0,0 +1,44 @@
+	/*
+  ___________CODED BY SUBHADEEP MANDAL______________
+  
+  LICENSING:
+  GNU General Public License v3.0  .....Dated: 29.3.2021
+  (c) 2021 Guava_Slice. All rights reserved.
+  
+  OVERVIEW
+  After the decimal point it finds 2 repeating numbers it will break
+  and approx it at that very point
+  
+  EXPECTATIONS from this approximation
+		1.0000001 ⇒ 1
+		6.0299999 ⇒ 6.03
+		0.9999999 ⇒ 1
+    
+    DRAWBACK
+    1.000569 ⇒ 1
+    If you don't want this kind of approximations don't use it
+	*/
+
+function Fixing_Mantisa_Of(sampul_str) {
+	var dgtcount=0;
+	var mantissaPass = false;
+
+	for(dgt=0; dgt<sampul_str.length; dgt++) {
+
+		if (sampul_str[dgt] == "."){
+			mantissaPass = true;
+			continue;
+		}
+
+		if (mantissaPass) {
+			/*_____Checks from where 3part repition starts in mantissa and then returns that index____*/
+			if(sampul_str[dgt] == sampul_str[dgt+1] && sampul_str[dgt] == sampul_str[dgt+2])
+				break;
+			dgtcount++;
+		}
+	}
+
+	sampul_str = Number(sampul_str).toFixed(dgtcount);
+
+	return sampul_str;
+}
