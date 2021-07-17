856d29ea7e45e49ec658123af7cb65a27f2a1ce0
Subhadeep Mandal <ultrasubha@gmail.com>
2021-07-17 23:42:40 +0530

    Create sem1.c

---
+++ C/sem1.c
@@ -0,0 +1,101 @@
+https://ideone.com/5uFKAX // Assignment for xerox bill
+https://ideone.com/Q3evaA //temperature
+https://ideone.com/yW6vK2 // 12345 divide into 123 and 45
+https://ideone.com/4Zaupw // To check the set for ECE assignment
+https://ideone.com/ckOgYO // Area of square,circle
+https://ideone.com/DzOg0a // To check square of a number
+https://ideone.com/C2hGJm // Multiplication Table
+https://ideone.com/I1GDgI // To check Factorial
+https://ideone.com/cPdgMD // To check 3 digit Armstrong number
+https://ideone.com/OHjVyD // All Armstrong number between 1 and 500
+https://ideone.com/t5Sfpm // Mam's version of  3 digit Armstrong number
+https://ideone.com/YOxuoC // sum of individual digit
+https://ideone.com/2y3cOi  // Mam's version of  sum of individual digit
+https://ideone.com/WYkaHL // Mam's version to reverse the number
+http://ideone.com/9mRjdp // Shobhon's version to reverse the number
+https://ideone.com/i2tIB3 // Reverse using Recursion
+https://ideone.com/GHATUV // Palindrome number
+https://ideone.com/F3G91x //doubt
+https://ideone.com/uz8ex8 // square and cube with goto
+https://ideone.com/37eMsW // Prime or composite number
+https://ideone.com/4aJu6T // Factors of a number
+https://ideone.com/0vO4kW //compilation with a function
+https://ideone.com/oCoBhU // Table with a function
+https://ideone.com/IRNdcD //Factorial with recurssive function
+https://ideone.com/uiE5sr //Power by recurssive function
+https://ideone.com/F0A8Zu //Prime or not by recurssive function
+https://ideone.com/yM0NiJ //Fibronancii series by recurssive function
+https://ideone.com/dF3YO0 //storage classes
+https://ideone.com/AHgRJL //auto variable
+https://ideone.com/8qpDx1 //register variable
+https://ideone.com/pDV1eg // static variable
+https://ideone.com/FVCI4Q //global variable
+https://ideone.com/36H77e //Array
+https://ideone.com/zzrBML //Copy with array
+https://ideone.com/ey8K4K // Squaring with array
+https://ideone.com/KriZft // Even no. with array
+https://ideone.com/8tBwpp //Average with array
+https://ideone.com/FOhFS5 //Reverse of an array
+https://ideone.com/54rp5d //Check roll number(array)
+https://ideone.com/knwk86 //Maximum and Minimum in an array
+https://ideone.com/UD8ljK // Enter or Delete a new element in an array
+https://ideone.com/AfNLYC //string
+https://ideone.com/ooDZ2e //string2
+https://ideone.com/mvd6wP //Count number of characters
+https://ideone.com/K5Lw2L //2D arrays
+https://ideone.com/sST7Fz // initializing 2D array from scanf
+https://ideone.com/KXkJQD // WAP to multiply two arrays
+https://ideone.com/OmAWiJ // WAP to substract two matrices
+
+https://ideone.com/cARj0X //  WAP to multiply two matrices
+https://ideone.com/GtMoSu // Sum of Diagonal elements of matrix
+//Transpose
+https://ideone.com/pB0zQG //function with arrays(vowels)
+https://ideone.com/UYDQWF //Pointers
+https://ideone.com/Us2eHH //search elements with pointers
+https://ideone.com/3WbTuW // squaring array with pointers
+https://ideone.com/f3VuQG //Call by refference
+https://ideone.com/XjNvoC //  maximum value in array using pointer to array
+https://ideone.com/K4Ikyt // calculate cube of value using call by reference and call by value
+https://ideone.com/uLOhft // Dynamic memory allocation
+https://ideone.com/Ks1Tdw // strings
+https://ideone.com/xxuAhH // reversing without strrev
+https://ideone.com/pJdDk1 // copying 1 string to another without strcpy
+https://ideone.com/yxbQ6c // convert uppercase to lowercase without strupr
+https://ideone.com/05YgMc // concatenation of 2 string without strcat
+https://ideone.com/NVU9rA //palindrome string
+https://ideone.com/4eKv5s //Binary search
+https://ideone.com/UONzDX //Binary search using pointer
+https://ideone.com/ckj3Vz // structure example
+https://ideone.com/L50CvL //local+global for structure
+https://ideone.com/u49eqS // structure with arrays
+https://ideone.com/IClvUR // nested structure
+https://ideone.com/Zuz64W // finding maximum of an element in structure
+https://ideone.com/k8L5cY //structure with pointers
+https://ideone.com/Ikz8QA // union
+https://ideone.com/MWjPw0 // atoi,atol etc (to be corrected)
+https://ideone.com/MRMKjE // ftoa
+https://ideone.com/dRpNNS // arrays with union
+https://ideone.com/r33rG8 //Andrew string sum
+https://ideone.com/1axXDM // Andrew string combinations
+
+https://www.geeksforgeeks.org/cc-preprocessors/
+
+https://saurabhdaware.github.io/text-to-handwriting/#how-to-add-handwriting 
+
+MY PROGRAMS:
+
+https://ideone.com/PTBZ5L //Square root
+https://ideone.com/9gZX9K //Coin flip simulation
+https://ideone.com/UoMzQx //The perfect numbers are
+https://ideone.com/wApNza //Kunal Armstrong Q1
+https://ideone.com/iSiWoe //Kunal Armstrong Q2
+https://ideone.com/h1rikt // power of a number
+https://ideone.com/e5Rcev // Check the number of digits
+https://ideone.com/9UoD0C // Armstrong number re-envisioned (different ideology)
+https://ideone.com/lqcLvi //string to characters and counting
+https://ideone.com/GFgIIq // Shobon's version of counting characters in a string
+https://ideone.com/WG8Ngz // string slicing
+https://ideone.com/u60bgj // Ramanujan numbers
+https://stackoverflow.com/questions/53182909/c-program-for-ramanujan-like-number-series
+
