ace4c91cfb82cad4a5abbabc3a5bc0f42a9d2d72
Subhadeep Mandal <ultrasubha@gmail.com>
2021-11-23 22:23:45 +0530

    Create ModifyString.py

---
+++ Python/HackerRank/ModifyString.py
@@ -0,0 +1,18 @@
+'''
+MODIFY A STRING WITH A PARTICULAR CHARACTER AT A PARTICULAR POSITION
+INPUT:
+abracadabra
+5 k
+OUTPUT:
+abrackdabra
+'''
+
+def mutate_string(string, position, character):
+    string = string[:position] + character + string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
