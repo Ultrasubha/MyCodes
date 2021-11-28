e3719ee55d7344f59bd63306a985ab94836e15d7
Subhadeep Mandal <ultrasubha@gmail.com>
2021-11-29 00:35:48 +0530

    Create Q1.cpp

---
+++ CPP/Competition/Hexaviewtech/Q1.cpp
@@ -0,0 +1,19 @@
+/*
+Given two arrays, Array1 and Array2, of equal size S, the objective is to track down the
+least value of Array1[0] * Array2[0] + Array1[1] * Array2[1] +…+ Array1[S-1] *
+Array2[S-1]. Shuffling of elements of arrays are allowed.
+Test Case 1
+Inputs:
+Array1[] = { 3, 1, 1 }
+Array2[] = { 5, 4, 6 }
+Output: 23
+Test Case 2
+Inputs:
+Array1[] = { 6, 1, 9, 5, 4 }
+Array2[] = { 3, 4, 8, 2, 4 }
+Output: 80
+
+Coded by Subhadeep Mandal
+Date:28.11.2021
+Question Source : https://github.com/prakartigoel24/Coding-Round-Questions/blob/main/Hexaview%20Technologies/
+*/
