163967306597a0f1b543369d01d426836d074155
Subhadeep Mandal <ultrasubha@gmail.com>
2021-12-09 13:04:49 +0530

    fixed

--- Python/CaesarCipher.py
+++ Python/CaesarCipher.py
@@ -6,21 +6,17 @@ LICENSING:
   GNU General Public License v3.0  .....Dated: 8-12-2021
   (c) 2021 Guava_Slice. All rights reserved.
 '''
-
 lst = [ord(i) for i in input("Enter your sentence\n")]
- 
 offset =  int(input("shift amount :"))
-lst = list(map(lambda x: x+offset if(x != 32) else 0, lst))
-for i in range(len(lst)):
-	if lst[i]>122:
-		lst[i] = 96 + (lst[i]-122)
-	elif lst[i]<65:
-		lst[i] = 91 - (65-lst[i])
-
-str="\n"
-for i in range(len(lst)):
-	if lst[i] != 0:
-		str+=chr(lst[i])
+msg="\n"
+for i in lst:
+	if i==32:
+		msg+=" "
+	elif i>=64 and i<=90:
+		msg+= chr((i + offset -65) % 26 + 65)
+	elif i>=97 and i<=122:
+		msg+= chr((i + offset - 97) % 26 + 97)
 	else:
-		str+=" "
-print(str)
+		msg = "\nPlease enter only alphanumeric characters"
+		break
+print(msg)
