e67352301ca4c05fcdd25606ff89e8b84ce0ac6b
Subhadeep Mandal <ultrasubha@gmail.com>
2021-12-16 11:22:18 +0530

    Create pass_by_value.c

---
+++ DSA/pass_by_value.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int x,int y){
+    int temp=x;
+    x=y;
+    y=temp;
+}
+
+int main()
+{
+   int a=10,b=20;
+   printf("a is %d and b is %d\n",a,b);
+   swap(a,b);
+   printf("After swapping,\na is %d and b is %d",a,b);
+    return 0;
+}
