2a5f15a00cf33aa784e7ccb18a06f6a30342d014
Subhadeep Mandal <ultrasubha@gmail.com>
2022-02-05 20:48:46 +0530

    Add files via upload

---
+++ Java/Codechef.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scan = new Scanner(System.in);
+        int N = 0, K = 0;
+        String S = "";
+        int T = scan.nextInt();
+        scan.nextLine();
+
+        for (int t = 0; t < T; t++) {
+            N = scan.nextInt();
+            K = scan.nextInt();
+            scan.nextLine();
+            S = scan.nextLine();
+
+            if (isPalindrome(S, N, K))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+        scan.close();
+    }
+
+    static boolean isPalindrome(String str, int N, int K) {
+        int i = 0, j = str.length() - 1, count = 0;
+
+        while (i < j) {
+
+            // Mismatch
+            if (str.charAt(i) != str.charAt(j))
+                count++;
+
+            i++;
+            j--;
+        }
+
+        if (count == K)
+            return true;
+        else if (count > K)
+            return false;
+        else {
+            K -= count;
+            if (K % 2 == 0)
+                return true;
+            else {
+                if (N % 2 == 0)
+                    return false;
+                else
+                    return true;
+            }
+        }
+    }
+}
