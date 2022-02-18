5bbbd19674ea0cafa306271807bf6509fb121b66
Subhadeep Mandal <ultrasubha@gmail.com>
2022-02-18 15:29:17 +0530

    Create StringFormatting.java

---
+++ Java/StringFormatting.java
@@ -0,0 +1,11 @@
+public class StringFormatting {
+        public static void main(String[] args) {
+                int iVar = 2435;
+
+                System.out.printf("%6d%n", iVar);
+                System.out.printf("%06d%n", iVar);
+                System.out.printf("%+06d%n", iVar);
+                System.out.printf("%+,06d%n", iVar);
+                System.out.printf("%-,6d%n", iVar);
+        }
+}
