4d4ac7e81abb52e4641c2c3b824896864a2466ee
Subhadeep Mandal <ultrasubha@gmail.com>
2023-10-25 09:57:08 +0530

    s

--- Python/practice.py
+++ Python/practice.py
@@ -1,13 +1,5 @@
-from pynput.keyboard import Key, Listener
-
-def show(key):
-
-	print('\nYou Entered {0}'.format( key))
-
-	if key == Key.delete:
-		# Stop listener
-		return False
-
-# Collect all event until released
-with Listener(on_press = show) as listener:
-	listener.join()
+#Simple Password Generator, Coded by Subhadeep Mandal.As per IBM password policy [Uppercase] + [Lowercase] + [numerals] + [special characters]
+import random
+characters = lambda x,y: [chr(i) for i in range(x,y)]
+lst = characters(65,91) + characters(97,123) + characters(48,58) + ["!", "@", "#", "$", "%", "^", "&", "*"]
+print("Your password : ","".join(random.sample(lst,int(input("Password Length? : ")))))
