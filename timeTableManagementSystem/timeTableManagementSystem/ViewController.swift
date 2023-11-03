44d7b608d88ac7d94aaeabdd215b4d4aceede71c
SUBHADEEP MANDAL <ultrasubha@gmail.com>
2023-11-03 10:51:25 +0530

    Add files via upload

---
+++ timeTableManagementSystem/timeTableManagementSystem/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  timeTableManagementSystem
+//
+//  Created by Student on 03/11/23.
+//  Copyright © 2023 lpu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    /*var day: String
+    var time: String
+    var subject: String
+    
+    init(day: String, time: String, subject: String){
+        self.day = day
+        self.time = time
+        self.subject = subject
+    }*/
+
+}
+
