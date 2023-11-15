47c59865255e556b99c9f3adc02626dc34bc7129
Subhadeep Mandal <ultrasubha@gmail.com>
2023-11-15 10:46:04 +0530

    sds

--- Python/practice.py
+++ Python/practice.py
@@ -1,5 +1,32 @@
 #Simple Password Generator, Coded by Subhadeep Mandal.As per IBM password policy [Uppercase] + [Lowercase] + [numerals] + [special characters]
-import random
+'''import random
 characters = lambda x,y: [chr(i) for i in range(x,y)]
 lst = characters(65,91) + characters(97,123) + characters(48,58) + ["!", "@", "#", "$", "%", "^", "&", "*"]
-print("Your password : ","".join(random.sample(lst,int(input("Password Length? : ")))))
+print("Your password : ","".join(random.sample(lst,int(input("Password Length? : ")))))'''
+def answer(area):
+    res = []
+    while (area > 0):
+        biggest_square_side = int(area ** 0.5)
+        biggest_square = biggest_square_side ** 2
+        area -= biggest_square
+        res.append(biggest_square)
+    return res
+
+def solve(arr,num):
+    rt = int(num**0.5)
+    sq = rt*rt
+    arr.append(sq)
+    if num>1:
+        return solve(arr,num-sq)
+    else:
+        if arr[-1]==0:
+            arr.pop()
+        return arr
+        
+def solution(area):
+    arr = []
+    return solve(arr,area)
+
+for i in range(1,1000000):
+    if answer(i)!=solution(i):
+        print(i,answer(i),solution(i))
--- Python/practice1.py
+++ Python/practice1.py
@@ -1,29 +1,20 @@
-'''
-Agenda
-1) File Handling
-2) Regex
-3) Date Time
-4) OS
-5) map, lambda expression
-6) Dictionary
-7) 
-8) 
-9) 
-10) 
-11) 
-12) 
-13) 
-14)
-15)
-16)
-17)
-18)
-19)
-'''
+#Use verify [file] to test your solution and see how it does.
+#When you are finished editing your code, use submit [file] to submit your answer.
+#If your solution passes the test cases, it will be removed from your home folder.
 
-file = open("D:\_WorkSpace\MyCodes\Python\practiceText.txt", "w")
-s = "Hi, this is me, Subhadeep.\nA Simple guy with simple dreams.\nWanting to develop games for all to play and enjoy.\nIf you ever comes accross a guy wanting to be a game developer in Japan, rest assured you just met Subhadeep"
-file.write(s)
-file.close()
-import re
-print(re.findall("^H.*", s))
+def solve(arr,num):
+    rt = int(num**0.5)
+    sq = rt*rt
+    arr.append(sq)
+    if num>1:
+        return solve(arr,num-sq)
+    else:
+        if arr[-1]==0:
+            arr.pop()
+        return arr #",".join(map(str,arr))
+        
+def solution(area):
+    arr = []
+    return solve(arr,area)
+
+print(solution(4))
