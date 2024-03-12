26183965def1fa3814dd03d107680a2e7c4e718a
Subhadeep Mandal <ultrasubha@gmail.com>
2024-03-12 16:04:37 +0530

    .

--- Python/Automate/automate.py
+++ Python/Automate/automate.py
@@ -1,6 +1,7 @@
 import time
 import pyautogui
 import webbrowser
+from datetime import datetime
 
 slack_link = "https://app.slack.com/client/TAYFNNJ1K/C023098AEF7"
 webbrowser.open(slack_link)
@@ -9,5 +10,7 @@ time.sleep(7)
 x_coordinate = 784
 y_coordinate = 909
 pyautogui.click(x_coordinate, y_coordinate)
-# pyautogui.write("haha")
-# pyautogui.press("enter")
+with open("yesterday.txt", "r") as file:
+    content = file.read()
+    pyautogui.write("Update@*" + datetime.today().strftime('%d-%m-%Y') + "*\n\n" + content)
+pyautogui.press("enter")
---
+++ Python/date_time.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+today_date = datetime.today().strftime('%d-%m-%Y')
+print(today_date)
